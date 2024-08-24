385be00beb297e1d6f274e691873d4ac1b8fabb4
Yuntai Song <songyuntai704@gmail.com>
2024-08-24 18:01:13 -0500

    Update __init__.py

--- src/qmc_tfim/__init__.py
+++ src/qmc_tfim/__init__.py
@@ -1,4 +1,4 @@
-from .hamiltonian import TFIM
+from .hamiltonian import TFIM, Hamiltonian
 from .lattice import lattice_bond_spins
 from .qmc_state import BinaryGroundState, BinaryThermalState
 from .updates import mc_step, mc_step_beta
