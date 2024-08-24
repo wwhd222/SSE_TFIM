5f345b4919c953fa9ab9f5f7ce5d4674361b1626
Yuntai Song <songyuntai704@gmail.com>
2024-08-24 18:26:02 -0500

    Update qmc_state.py

--- src/qmc_tfim/qmc_state.py
+++ src/qmc_tfim/qmc_state.py
@@ -47,24 +47,20 @@ class BinaryGroundState(AbstractGroundState):
         return cls(left_config, right_config, operator_list)
 
 class BinaryThermalState(AbstractThermalState):
-    def __init__(self, left_config: np.ndarray, right_config: np.ndarray, operator_list: List[Tuple[int, int]]):
-        super().__init__(2, left_config.dim)
-        assert left_config is not right_config, "left_config and right_config can't be the same array!"
-
-        self.left_config = left_config
-        self.right_config = right_config
-        self.propagated_config = left_config.copy()
+    def __init__(self, H: TFIM, M: int, operator_list: List[Tuple[int, int]]):
+        super().__init__(2, H.N)  # 使用 H.N 而不是 H.dim，因为 TFIM 继承自 Hamiltonian
+        self.left_config = np.random.choice([0, 1], size=H.Ns)
+        self.right_config = self.left_config.copy()
+        self.propagated_config = self.left_config.copy()
         self.operator_list = operator_list
 
-        len_config = len(left_config)
-        len_op_list = len(operator_list)
-        len_total = 4 * len_op_list
+        len_list = 4 * len(operator_list)
+        self.linked_list = np.zeros(len_list, dtype=int)
+        self.leg_types = np.zeros(len_list, dtype=bool)
+        self.associates = [(0, 0, 0) for _ in range(len_list)]
 
-        self.linked_list = np.zeros(len_total, dtype=int)
-        self.leg_types = np.zeros(len_total, dtype=bool)
-        self.associates = [(0, 0, 0) for _ in range(len_total)]
-        self.first = np.zeros(len_config, dtype=int)
-        self.last = np.zeros(len_config, dtype=int)
+        self.first = np.zeros(H.Ns, dtype=int)
+        self.last = np.zeros(H.Ns, dtype=int)
 
     @classmethod
     def from_hamiltonian(cls, H: Hamiltonian, cutoff: int):
